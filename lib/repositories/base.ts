6c43b725414fbf1734ce5b634676bd0093312979
Daniel Kurka <kurka.daniel@gmail.com>
2022-10-31 20:15:06 -0700

    Fix typing in OrderRepository.

--- lib/repositories/base.ts
+++ lib/repositories/base.ts
@@ -3,5 +3,4 @@ import { OrderEntity } from '../entities/index'
 export interface BaseOrdersRepository {
   getByHash: (hash: string) => Promise<OrderEntity | undefined>
   putOrderAndUpdateNonceTransaction: (order: OrderEntity) => Promise<void>
-  put: (order: OrderEntity) => Promise<void>
 }
--- lib/repositories/orders-repository.ts
+++ lib/repositories/orders-repository.ts
@@ -8,10 +8,8 @@ import { BaseOrdersRepository } from './base'
 export class DynamoOrdersRepository implements BaseOrdersRepository {
   private static ordersTable: Table
   private static nonceTable: Table
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  private static orderEntity: any
-  private static nonceEntity: any
-  /* eslint-enable @typescript-eslint/no-explicit-any */
+  private static orderEntity: Entity<{}>
+  private static nonceEntity: Entity<{}>
 
   static initialize(documentClient: DocumentClient) {
     this.ordersTable = new Table({
