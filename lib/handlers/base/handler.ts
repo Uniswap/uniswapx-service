9ed2d424cbd867eab410b49a92d1fd8b58d83e8e
Lint Action <lint-action@samuelmeuli.com>
2022-11-03 17:46:09 +0000

    Fix code style issues with Prettier

--- lib/handlers/base/handler.ts
+++ lib/handlers/base/handler.ts
@@ -39,7 +39,7 @@ export type ErrorResponse = {
 
 export abstract class Injector<CInj, RInj extends BaseRInj, ReqBody, ReqQueryParams> {
   private containerInjected: CInj | undefined
-  public constructor(protected injectorName: string) { }
+  public constructor(protected injectorName: string) {}
 
   public async build() {
     this.containerInjected = await this.buildContainerInjected()
@@ -78,8 +78,8 @@ export abstract class APIGLambdaHandler<CInj, RInj extends BaseRInj, ReqBody, Re
     private readonly handlerName: string,
     private readonly injectorPromise: Promise<Injector<CInj, RInj, ReqBody, ReqQueryParams>>
   ) {
-    checkDefined(handlerName);
-    checkDefined(injectorPromise);
+    checkDefined(handlerName)
+    checkDefined(injectorPromise)
   }
 
   get handler(): APIGatewayProxyHandler {
@@ -149,7 +149,7 @@ export abstract class APIGLambdaHandler<CInj, RInj extends BaseRInj, ReqBody, Re
 
       const { requestId: id } = requestInjected
 
-        ; ({ log } = requestInjected)
+      ;({ log } = requestInjected)
 
       let statusCode: number
       let body: Res
@@ -176,7 +176,7 @@ export abstract class APIGLambdaHandler<CInj, RInj extends BaseRInj, ReqBody, Re
           }
         } else {
           log.info({ requestBody, requestQueryParams }, 'Handler returned 200')
-            ; ({ body, statusCode } = handleRequestResult)
+          ;({ body, statusCode } = handleRequestResult)
         }
       } catch (err) {
         log.error({ err }, 'Unexpected error in handler')
@@ -222,10 +222,10 @@ export abstract class APIGLambdaHandler<CInj, RInj extends BaseRInj, ReqBody, Re
     log: Logger
   ): Promise<
     | {
-      state: 'valid'
-      requestBody: ReqBody
-      requestQueryParams: ReqQueryParams
-    }
+        state: 'valid'
+        requestBody: ReqBody
+        requestQueryParams: ReqQueryParams
+      }
     | { state: 'invalid'; errorResponse: APIGatewayProxyResult }
   > {
     /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
--- test/handlers/base.test.ts
+++ test/handlers/base.test.ts
@@ -1,52 +1,57 @@
-import { ObjectSchema } from "joi";
-import { APIGLambdaHandler, BaseRInj, ErrorResponse, HandleRequestParams, Response } from "../../lib/handlers/base/handler";
+import { ObjectSchema } from 'joi'
+import {
+  APIGLambdaHandler,
+  BaseRInj,
+  ErrorResponse,
+  HandleRequestParams,
+  Response,
+} from '../../lib/handlers/base/handler'
 
 export interface ContainerInjected {
-    foo: string,
+  foo: string
 }
 
 export interface RequestInjected extends BaseRInj {
-    bar: string,
+  bar: string
 }
 
 export interface ReqBody {
-    baz: string
+  baz: string
 }
 
-export interface ReqQueryParams { }
+export interface ReqQueryParams {}
 
 export interface Res {
-    foobar: string
+  foobar: string
 }
 
-
-
-class ThrowingTestHandler extends APIGLambdaHandler<ContainerInjected,
-    RequestInjected,
-    ReqBody,
-    ReqQueryParams,
-    Res> {
-
-    public handleRequest(_params: HandleRequestParams<ContainerInjected, RequestInjected, ReqBody, ReqQueryParams>): Promise<ErrorResponse | Response<Res>> {
-        throw new Error("Method not implemented.");
-    }
-    protected requestBodySchema(): ObjectSchema<any> | null {
-        throw new Error("Method not implemented.");
-    }
-    protected requestQueryParamsSchema(): ObjectSchema<any> | null {
-        throw new Error("Method not implemented.");
-    }
-    protected responseBodySchema(): ObjectSchema<any> | null {
-        throw new Error("Method not implemented.");
-    }
+class ThrowingTestHandler extends APIGLambdaHandler<ContainerInjected, RequestInjected, ReqBody, ReqQueryParams, Res> {
+  public handleRequest(
+    _params: HandleRequestParams<ContainerInjected, RequestInjected, ReqBody, ReqQueryParams>
+  ): Promise<ErrorResponse | Response<Res>> {
+    throw new Error('Method not implemented.')
+  }
+  protected requestBodySchema(): ObjectSchema<any> | null {
+    throw new Error('Method not implemented.')
+  }
+  protected requestQueryParamsSchema(): ObjectSchema<any> | null {
+    throw new Error('Method not implemented.')
+  }
+  protected responseBodySchema(): ObjectSchema<any> | null {
+    throw new Error('Method not implemented.')
+  }
 }
 
-describe("APIGLambdaHandler", () => {
-    it("Should throw if handlerName is not defined", () => {
-        expect(() => { new ThrowingTestHandler(undefined as any, {} as any); }).toThrow()
-    })
+describe('APIGLambdaHandler', () => {
+  it('Should throw if handlerName is not defined', () => {
+    expect(() => {
+      new ThrowingTestHandler(undefined as any, {} as any)
+    }).toThrow()
+  })
 
-    it("Should throw if injectorPromise is not defined", () => {
-        expect(() => { new ThrowingTestHandler("handlerName", null as any); }).toThrow()
-    })
-});
+  it('Should throw if injectorPromise is not defined', () => {
+    expect(() => {
+      new ThrowingTestHandler('handlerName', null as any)
+    }).toThrow()
+  })
+})
