8e918db31dd35cddde3cd109155f167c42b319a1
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2022-11-09 09:04:12 -0500

    udpate comment

--- lib/util/nonce.ts
+++ lib/util/nonce.ts
@@ -4,8 +4,9 @@ import { ethers } from 'ethers'
  * uses crypto.randomBytes() under the hood to generate a 'cryptographically strong'
  *  random data of 28 bytes and prefix that with gouda specific 3-byte value
  * (in total 248 bits, which is the number of words in the Permit2 unorderd nonceBitmap).
- *  We then left shin total ift by 8 bits to form the complete uint256 nonce value.
- *
+ *  We then left shin total ift by 8 bits to form the complete uint256 nonce value; we do
+ *    this because we want the first nonce to land on the word boundary to save gas (clean sstore
+ *     for the next 256 nonce value)
  * @returns random nonce generated for new wallet addresses
  */
 export function generateRandomNonce(): string {
