7c5fd91f631064c9429e1f27f3cdcb10fdb5ff45
raghava-pamula <106625855+raghava-pamula@users.noreply.github.com>
2022-11-14 14:11:55 -0500

    remove npm

--- bin/app.ts
+++ bin/app.ts
@@ -63,7 +63,7 @@ export class APIPipeline extends Stack {
       },
       commands: [
         'git config --global url."https://${GH_TOKEN}@github.com/".insteadOf ssh://git@github.com/',
-        'echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc && yarn ci',
+        'echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc && yarn install --frozen-lockfile',
         'yarn build',
         'npx cdk synth',
       ],
