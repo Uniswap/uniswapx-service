f5146a3dcb6fa596c0fb92c14a1fb840fadb479b
raghava-pamula <106625855+raghava-pamula@users.noreply.github.com>
2022-11-14 14:35:36 -0500

    moved yarn install to its own line in pipeline (#44)

--- bin/app.ts
+++ bin/app.ts
@@ -63,7 +63,8 @@ export class APIPipeline extends Stack {
       },
       commands: [
         'git config --global url."https://${GH_TOKEN}@github.com/".insteadOf ssh://git@github.com/',
-        'echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc && yarn install --frozen-lockfile',
+        'echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc',
+        'yarn install --frozen-lockfile',
         'yarn build',
         'npx cdk synth',
       ],
@@ -145,9 +146,9 @@ export class APIPipeline extends Stack {
       },
       commands: [
         'git config --global url."https://${GH_TOKEN}@github.com/".insteadOf ssh://git@github.com/',
-        'echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc && yarn install --frozen-lockfile',
+        'echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc',
         'echo "UNISWAP_API=${UNISWAP_API}" > .env',
-        'yarn install',
+        'yarn install --frozen-lockfile',
         'yarn build',
         'yarn run integ-test',
       ],
