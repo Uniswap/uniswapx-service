3bddd75469dc99e68c81a2a2e44797a409495a65
Riley Campbell <riley.campbell@uniswap.org>
2023-02-01 16:52:30 -0500

    feat: added more tests

--- lib/handlers/get-orders/schema/index.ts
+++ lib/handlers/get-orders/schema/index.ts
@@ -36,6 +36,7 @@ export const OrderResponseEntryJoi = Joi.object({
   orderStatus: FieldValidator.isValidOrderStatus(),
   orderHash: FieldValidator.isValidOrderHash(),
   offerer: FieldValidator.isValidEthAddress(),
+  txHash: FieldValidator.isValidTxHash(),
 })
 
 export const GetOrdersResponseJoi = Joi.object({
