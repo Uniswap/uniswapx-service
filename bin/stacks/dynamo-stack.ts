a443dfad7d95812a33ec7fac31bd2837f08f734e
Riley Campbell <riley.campbell@uniswap.org>
2023-02-02 11:58:45 -0500

    feat: add GSI all filler-offerer-orderStatus-createdAt

--- bin/stacks/dynamo-stack.ts
+++ bin/stacks/dynamo-stack.ts
@@ -244,6 +244,19 @@ export class DynamoStack extends cdk.NestedStack {
       projectionType: aws_dynamo.ProjectionType.ALL,
     })
 
+    this.ordersTable.addGlobalSecondaryIndex({
+      indexName: `${TABLE_KEY.FILLER}_${TABLE_KEY.OFFERER}_${TABLE_KEY.ORDER_STATUS}-${TABLE_KEY.CREATED_AT}-all`,
+      partitionKey: {
+        name: `${TABLE_KEY.FILLER}_${TABLE_KEY.OFFERER}_${TABLE_KEY.ORDER_STATUS}`,
+        type: aws_dynamo.AttributeType.STRING,
+      },
+      sortKey: {
+        name: TABLE_KEY.CREATED_AT,
+        type: aws_dynamo.AttributeType.NUMBER,
+      },
+      projectionType: aws_dynamo.ProjectionType.ALL,
+    })
+
     /* Nonces Table
      * This is needed because we want to do strongly-consistent reads on the nonce value,
      *  which is not possible to do on secondary indexes (if we work with only the Orders table).
