fe1edd961354f8bf44fc2958a87863b748207894
Eric Zhong <eric.zhong@uniswap.org>
2023-05-22 12:16:27 -0400

    Do not dump entire abi of quoter into logs

--- lib/handlers/check-order-status/injector.ts
+++ lib/handlers/check-order-status/injector.ts
@@ -45,7 +45,7 @@ export class CheckOrderStatusInjector extends SfnInjector<ContainerInjected, Req
     // TODO: use different reactor address for different order type
     const watcher = new EventWatcher(provider, REACTOR_ADDRESS_MAPPING[chainId as number][OrderType.DutchLimit])
 
-    log.info({ quoter: quoter, watcher: watcher, quoterAddr: quoter.orderQuoterAddress }, 'getRequestInjected')
+    log.info({ quoterAddr: quoter.orderQuoterAddress }, 'getRequestInjected')
     return {
       log,
       chainId: event.chainId as number,
