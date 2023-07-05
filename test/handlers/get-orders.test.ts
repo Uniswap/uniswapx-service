6517ba6512606a4550fbe2fb527c62edcfe89998
Eric Zhong <eric.zhong@uniswap.org>
2023-07-05 13:03:17 -0400

    pass tests

--- test/handlers/get-orders.test.ts
+++ test/handlers/get-orders.test.ts
@@ -9,7 +9,7 @@ describe('Testing get orders handler.', () => {
       '0x1c33da80f46194b0db3398de4243d695dfa5049c4cc341e80f5b630804a47f2f52b9d16cb65b2a2d8ed073da4b295c7cb3ccc13a49a16a07ad80b796c31b283414',
     orderStatus: ORDER_STATUS.OPEN,
     orderHash: '0xa2444ef606a0d99809e1878f7b819541618f2b7990bb9a7275996b362680cae4',
-    offerer: '0x11E4857Bb9993a50c685A79AFad4E6F65D518DDa',
+    swapper: '0x11E4857Bb9993a50c685A79AFad4E6F65D518DDa',
     createdAt: 1667276283251,
     encodedOrder: '0xencoded000order',
     type: OrderType.Dutch,
@@ -32,7 +32,7 @@ describe('Testing get orders handler.', () => {
   // Creating mocks for all the handler dependencies.
   const getOrdersMock = jest.fn()
   const queryFiltersMock = {
-    offerer: MOCK_ORDER.offerer,
+    offerer: MOCK_ORDER.swapper,
     filler: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
     orderStatus: ORDER_STATUS.OPEN,
     sortKey: SORT_FIELDS.CREATED_AT,
@@ -113,14 +113,14 @@ describe('Testing get orders handler.', () => {
         { orderHashes: '0xbad_hash1,0xbad_hash2' },
         'Invalid input. Expected comma-separated order hashes, with a maximum of 50, each matching the pattern \\"^0x[0-9a-zA-Z]64$\\".","errorCode":"VALIDATION_ERROR"',
       ],
-      [{ offerer: '0xbad_address' }, 'VALIDATION ERROR: Invalid address'],
+      [{ swapper: '0xbad_address' }, 'VALIDATION ERROR: Invalid address'],
       [{ orderStatus: 'bad_status' }, 'must be one of [open, filled, cancelled, expired, error, insufficient-funds]'],
       [{ limit: 'bad_limit' }, 'must be a number'],
       [{ filler: '0xcorn' }, 'VALIDATION ERROR: Invalid address'],
       [{ sortKey: 'createdBy' }, 'must be [createdAt]'],
       [
         { sortKey: 'createdAt' },
-        '{"detail":"\\"value\\" must contain at least one of [orderHash, orderHashes, chainId, orderStatus, offerer, filler]","errorCode":"VALIDATION_ERROR"}',
+        '{"detail":"\\"value\\" must contain at least one of [orderHash, orderHashes, chainId, orderStatus, swapper, filler]","errorCode":"VALIDATION_ERROR"}',
       ],
       [{ sort: 'foo(bar)' }, '"foo(bar)\\" fails to match the required pattern'],
       [{ cursor: 1 }, 'must be a string'],
@@ -132,8 +132,8 @@ describe('Testing get orders handler.', () => {
         '{"detail":"\\"desc\\" must be a boolean","errorCode":"VALIDATION_ERROR"}',
       ],
       [
-        { chainId: 1, offerer: '0x11E4857Bb9993a50c685A79AFad4E6F65D518DDa' },
-        '{"detail":"Querying with both offerer and chainId is not currently supported.","errorCode":"VALIDATION_ERROR"}',
+        { chainId: 1, swapper: '0x11E4857Bb9993a50c685A79AFad4E6F65D518DDa' },
+        '{"detail":"Querying with both swapper and chainId is not currently supported.","errorCode":"VALIDATION_ERROR"}',
       ],
     ])('Throws 400 with invalid query param %p', async (invalidQueryParam, bodyMsg) => {
       const invalidEvent = {
@@ -151,7 +151,7 @@ describe('Testing get orders handler.', () => {
   describe('Testing invalid response validation.', () => {
     it.each([
       [{ orderHash: '0xbad_hash' }],
-      [{ offerer: '0xbad_address' }],
+      [{ swapper: '0xbad_address' }],
       [{ orderStatus: 'bad_status' }],
       [{ signature: '0xbad_sig' }],
       [{ encodedOrder: '0xencoded$$$order' }],
