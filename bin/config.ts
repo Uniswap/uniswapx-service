260ffad7f8fdae9b0adef13674795f9b04f39696
Riley Campbell <rileydcampbell@berkeley.edu>
2023-07-17 16:24:10 +0200

    lower wcu (#283)

--- bin/config.ts
+++ bin/config.ts
@@ -3,9 +3,9 @@ import { IndexCapacityConfig, TableCapacityConfig } from './stacks/dynamo-stack'
 
 export const PROD_INDEX_CAPACITY: IndexCapacityConfig = {
   orderStatus: { readCapacity: 20000, writeCapacity: 100 },
+  fillerOrderStatus: { readCapacity: 2000, writeCapacity: 100 },
   filler: { readCapacity: 2000, writeCapacity: 100 },
   offerer: { readCapacity: 2000, writeCapacity: 100 },
-  fillerOrderStatus: { readCapacity: 2000, writeCapacity: 1000 },
   fillerOfferer: { readCapacity: 2000, writeCapacity: 1000 },
   fillerOrderStatusOfferer: { readCapacity: 2000, writeCapacity: 1000 },
   offererOrderStatus: { readCapacity: 2000, writeCapacity: 1000 },
