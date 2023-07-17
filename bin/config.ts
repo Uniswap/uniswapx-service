6f4716db971db99d7edfc8d6ccaa9bbcc4840559
Riley Campbell <rileydcampbell@berkeley.edu>
2023-07-17 16:26:32 +0200

    lower wcu (#284)

--- bin/config.ts
+++ bin/config.ts
@@ -6,7 +6,7 @@ export const PROD_INDEX_CAPACITY: IndexCapacityConfig = {
   fillerOrderStatus: { readCapacity: 2000, writeCapacity: 100 },
   filler: { readCapacity: 2000, writeCapacity: 100 },
   offerer: { readCapacity: 2000, writeCapacity: 100 },
-  fillerOfferer: { readCapacity: 2000, writeCapacity: 1000 },
+  fillerOfferer: { readCapacity: 2000, writeCapacity: 100 },
   fillerOrderStatusOfferer: { readCapacity: 2000, writeCapacity: 1000 },
   offererOrderStatus: { readCapacity: 2000, writeCapacity: 1000 },
   chainId: { readCapacity: 20000, writeCapacity: 1000 },
