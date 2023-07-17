8ec4082fc5e243b2f4441434a172377f6e65df7a
Riley Campbell <rileydcampbell@berkeley.edu>
2023-07-17 09:38:27 +0200

    lower rcu

--- bin/config.ts
+++ bin/config.ts
@@ -3,7 +3,7 @@ import { IndexCapacityConfig, TableCapacityConfig } from './stacks/dynamo-stack'
 
 export const PROD_INDEX_CAPACITY: IndexCapacityConfig = {
   orderStatus: { readCapacity: 20000, writeCapacity: 100 },
-  offerer: { readCapacity: 2000, writeCapacity: 1000 },
+  offerer: { readCapacity: 2000, writeCapacity: 100 },
   filler: { readCapacity: 2000, writeCapacity: 1000 },
   fillerOrderStatus: { readCapacity: 2000, writeCapacity: 1000 },
   fillerOfferer: { readCapacity: 2000, writeCapacity: 1000 },
