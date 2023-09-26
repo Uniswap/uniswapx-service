16741eca57583967b64ee2704bd282e7c9744bd7
Riley Campbell <rileydcampbell@berkeley.edu>
2023-09-26 16:37:50 -0400

    chore: add support for beta s3 config.

--- lib/handlers/order-notification/injector.ts
+++ lib/handlers/order-notification/injector.ts
@@ -4,9 +4,10 @@ import { default as bunyan, default as Logger } from 'bunyan'
 import { checkDefined } from '../../preconditions/preconditions'
 import { WebhookProvider } from '../../providers/base'
 import { S3WebhookConfigurationProvider } from '../../providers/s3-webhook-provider'
-import { PRODUCTION_WEBHOOK_CONFIG_KEY, WEBHOOK_CONFIG_BUCKET } from '../../util/constants'
+import { BETA_WEBHOOK_CONFIG_KEY, PRODUCTION_WEBHOOK_CONFIG_KEY, WEBHOOK_CONFIG_BUCKET } from '../../util/constants'
 import { setGlobalLogger } from '../../util/log'
 import { setGlobalMetrics } from '../../util/metrics'
+import { STAGE } from '../../util/stage'
 import { DynamoStreamInjector } from '../base/dynamo-stream-handler'
 import { BaseRInj } from '../base/index'
 
@@ -21,10 +22,8 @@ export interface ContainerInjected {
 export class OrderNotificationInjector extends DynamoStreamInjector<ContainerInjected, RequestInjected> {
   public async buildContainerInjected(): Promise<ContainerInjected> {
     const stage = checkDefined(process.env['stage'], 'stage should be defined in the .env')
-    const webhookProvider = new S3WebhookConfigurationProvider(
-      `${WEBHOOK_CONFIG_BUCKET}-${stage}-1`,
-      PRODUCTION_WEBHOOK_CONFIG_KEY
-    )
+    const s3Key = stage === STAGE.BETA ? BETA_WEBHOOK_CONFIG_KEY : PRODUCTION_WEBHOOK_CONFIG_KEY
+    const webhookProvider = new S3WebhookConfigurationProvider(`${WEBHOOK_CONFIG_BUCKET}-${stage}-1`, s3Key)
     return { webhookProvider }
   }
 
--- lib/util/constants.ts
+++ lib/util/constants.ts
@@ -1,3 +1,4 @@
 export const WEBHOOK_CONFIG_BUCKET = 'order-webhook-notification-config'
 export const PRODUCTION_WEBHOOK_CONFIG_KEY = 'production.json'
+export const BETA_WEBHOOK_CONFIG_KEY = 'beta.json'
 export const NATIVE_ADDRESS = '0x0000000000000000000000000000000000000000'
