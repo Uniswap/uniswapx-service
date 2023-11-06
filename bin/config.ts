6a4f732680f05bde307c6e3b8983e4a4c4ccea6b
Riley Campbell <rileydcampbell@berkeley.edu>
2023-11-06 15:43:23 -0500

    chore: change billing option (#336)
    * chore: remove table level provisioning
    
    * chore: change billing option

--- bin/config.ts
+++ bin/config.ts
@@ -16,6 +16,6 @@ export const PROD_INDEX_CAPACITY: IndexCapacityConfig = {
 }
 
 export const PROD_TABLE_CAPACITY: TableCapacityConfig = {
-  order: { billingMode: BillingMode.PROVISIONED },
+  order: { billingMode: BillingMode.PAY_PER_REQUEST },
   nonce: { billingMode: BillingMode.PROVISIONED, readCapacity: 2000, writeCapacity: 1000 },
 }
