b6edd5c0167be1af49db67488cbb383473634d20
Robert Seitz <robert.seitz@uniswap.org>
2024-02-13 15:54:09 -0500

    trigger alarm on unsuccesful start

--- lib/compute/status.ts
+++ lib/compute/status.ts
@@ -1,6 +1,8 @@
+import { MetricUnits } from '@aws-lambda-powertools/metrics'
 import awssdk from 'aws-sdk'
 import { HEALTH_CHECK_PORT } from '../../bin/constants'
 import { log } from '../Logging'
+import { OnChainStatusCheckerMetricNames, powertoolsMetric as metrics } from '../Metrics'
 import { LimitOrdersRepository } from '../repositories/limit-orders-repository'
 import { HealthCheckServer } from './healthcheck'
 import { OnChainStatusChecker } from './on-chain-status-checker'
@@ -12,6 +14,12 @@ async function start() {
   await new OnChainStatusChecker(limitOrdersDb).pollForOpenOrders()
 }
 
-start().then(() => {
-  log.info('started')
-})
+start().then(
+  () => {
+    log.info('started')
+  },
+  (reject) => {
+    log.error('OnChainStatusChecker-Startup Unsuccessful', { message: reject })
+    metrics.singleMetric().addMetric(OnChainStatusCheckerMetricNames.LoopEnded, MetricUnits.Count, 1)
+  }
+)
