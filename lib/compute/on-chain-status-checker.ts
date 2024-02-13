f63f2df40d83b36032c11d840c3d08420d566bac
Robert Seitz <robert.seitz@uniswap.org>
2024-02-13 11:09:57 -0500

    lint

--- lib/compute/on-chain-status-checker.ts
+++ lib/compute/on-chain-status-checker.ts
@@ -51,7 +51,7 @@ export class OnChainStatusChecker {
         let openOrders = await this.dbInterface.getByOrderStatus(ORDER_STATUS.OPEN, BATCH_READ_MAX)
         do {
           const promises = await this.processOrderBatch(openOrders)
-          let results = await Promise.allSettled(promises)
+          const results = await Promise.allSettled(promises)
           statusLoopError += results.filter((p) => p.status === 'rejected').length
           totalCheckedOrders += openOrders.orders.length
         } while (
