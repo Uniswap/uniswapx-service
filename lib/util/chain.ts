b56051914e794511ea9826fafb46105f2b4411f7
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2024-03-29 11:48:37 -0400

    remove goerli

--- lib/util/chain.ts
+++ lib/util/chain.ts
@@ -1,6 +1,5 @@
 export enum ChainId {
   MAINNET = 1,
-  GÖRLI = 5,
   OPTIMISM = 10,
   ARBITRUM_ONE = 42161,
   POLYGON = 137,
@@ -9,4 +8,4 @@ export enum ChainId {
 
 // If you update SUPPORTED_CHAINS, ensure you add a corresponding RPC_${chainId} enviroment variable.
 // lib/config.py will require it to be defined.
-export const SUPPORTED_CHAINS = [ChainId.MAINNET, ChainId.GÖRLI, ChainId.POLYGON, ChainId.SEPOLIA]
+export const SUPPORTED_CHAINS = [ChainId.MAINNET, ChainId.POLYGON, ChainId.SEPOLIA]
