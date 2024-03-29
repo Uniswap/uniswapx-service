06260030b77dac5f0f86a58400f7d47b148b75e5
ConjunctiveNormalForm <zhiyuan.zach.yang@gmail.com>
2024-03-29 11:23:11 -0400

    add sepolia to supported chains

--- lib/util/chain.ts
+++ lib/util/chain.ts
@@ -4,8 +4,9 @@ export enum ChainId {
   OPTIMISM = 10,
   ARBITRUM_ONE = 42161,
   POLYGON = 137,
+  SEPOLIA = 11155111,
 }
 
 // If you update SUPPORTED_CHAINS, ensure you add a corresponding RPC_${chainId} enviroment variable.
 // lib/config.py will require it to be defined.
-export const SUPPORTED_CHAINS = [ChainId.MAINNET, ChainId.GÖRLI, ChainId.POLYGON]
+export const SUPPORTED_CHAINS = [ChainId.MAINNET, ChainId.GÖRLI, ChainId.POLYGON, ChainId.SEPOLIA]
