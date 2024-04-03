01e8c90fe7456d7b85e6ad759ace93608cfae19e
Robert Seitz <robert.seitz@uniswap.org>
2024-04-03 12:41:20 -0400

    add tests

--- lib/handlers/check-order-status/util.ts
+++ lib/handlers/check-order-status/util.ts
@@ -151,11 +151,7 @@ export const LIMIT_ORDERS_FILL_EVENT_LOOKBACK_BLOCKS_ON = (chainId: ChainId): nu
 }
 
 const watcherMap = new Map<string, EventWatcher>()
-export function getWatcher(
-  provider: ethers.providers.StaticJsonRpcProvider,
-  chainId: number,
-  orderType: OrderType
-): EventWatcher {
+export function getWatcher(provider: ethers.providers.StaticJsonRpcProvider, chainId: number, orderType: OrderType) {
   const reactorType = orderType === OrderType.Limit ? OrderType.Dutch : orderType
   if (!REACTOR_ADDRESS_MAPPING[chainId][reactorType]) {
     throw new Error(`No Reactor Address Defined in UniswapX SDK for chainId:${chainId}, orderType:${reactorType}`)
@@ -164,7 +160,7 @@ export function getWatcher(
   if (!watcherMap.get(mapKey)) {
     watcherMap.set(mapKey, new EventWatcher(provider, REACTOR_ADDRESS_MAPPING[chainId][reactorType] as string))
   }
-  return watcherMap.get(mapKey) as EventWatcher
+  return watcherMap.get(mapKey)
 }
 
 const providersMap = new Map<number, ethers.providers.StaticJsonRpcProvider>()
