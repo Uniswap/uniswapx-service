1a71b7005fa266ccec956b40c3cdd4e6997dd412
Cody Born <cody.born@uniswap.org>
2024-06-11 17:03:23 +0100

    Fix charts and alerts

--- lib/handlers/check-order-status/fill-event-logger.ts
+++ lib/handlers/check-order-status/fill-event-logger.ts
@@ -55,8 +55,10 @@ export class FillEventLogger {
     }
 
     // blocks until fill is the number of blocks between the fill event and the starting block number (need to add back the look back blocks)
-    const blocksUntilFill = fillEvent.blockNumber - (startingBlockNumber + this.fillEventBlockLookback(chainId))
-    metrics.putMetric(`OrderSfn-BlocksUntilFill-chain-${chainId}`, blocksUntilFill, Unit.Count)
+    if (startingBlockNumber != 0) {
+      const blocksUntilFill = fillEvent.blockNumber - (startingBlockNumber + this.fillEventBlockLookback(chainId))
+      metrics.putMetric(`OrderSfn-BlocksUntilFill-chain-${chainId}`, blocksUntilFill, Unit.Count)
+    }
     return settledAmounts
   }
 }
