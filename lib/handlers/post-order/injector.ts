a24e2b2781aed650a3ac75627dc9501b2f41c6b9
Eric Zhong <ezhong1900@gmail.com>
2024-11-06 20:24:14 -0500

    fix: remove kms initialization from injector (#489)
    * remove kms initialization from injector
    
    * fix tests
    
    * prettier
    
    * fix test to pass CI

--- lib/handlers/post-order/injector.ts
+++ lib/handlers/post-order/injector.ts
@@ -1,9 +1,6 @@
-import { KMSClient } from '@aws-sdk/client-kms'
-import { KmsSigner } from '@uniswap/signer'
 import { MetricsLogger } from 'aws-embedded-metrics'
 import { APIGatewayEvent, Context } from 'aws-lambda'
 import { default as Logger } from 'bunyan'
-import { checkDefined } from '../../preconditions/preconditions'
 import { setGlobalLogger } from '../../util/log'
 import { setGlobalMetrics } from '../../util/metrics'
 import { ApiInjector, ApiRInj } from '../base'
@@ -13,14 +10,7 @@ import { PostOrderRequestBody } from './schema'
 
 export class PostOrderInjector extends ApiInjector<PostContainerInjected, ApiRInj, PostOrderRequestBody, void> {
   public async buildContainerInjected(): Promise<PostContainerInjected> {
-    const kmsKeyId = checkDefined(process.env.KMS_KEY_ID, 'KMS_KEY_ID is not defined')
-    const region = checkDefined(process.env.REGION, 'REGION is not defined')
-    const cosigner = new KmsSigner(new KMSClient({ region: region }), kmsKeyId)
-    const cosignerAddress = await cosigner.getAddress()
-
-    return {
-      cosignerAddress,
-    }
+    return {}
   }
 
   public async getRequestInjected(
