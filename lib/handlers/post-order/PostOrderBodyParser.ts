540ab4dae447af1812f18bff04e5530023c8c8aa
Cody Born <cody.born@uniswap.org>
2024-11-26 17:37:39 +0900

    Track dutchv2 order latency

--- lib/handlers/post-order/PostOrderBodyParser.ts
+++ lib/handlers/post-order/PostOrderBodyParser.ts
@@ -17,6 +17,9 @@ import { Order } from '../../models/Order'
 import { PriorityOrder } from '../../models/PriorityOrder'
 import { RelayOrder } from '../../models/RelayOrder'
 import { PostOrderRequestBody } from './schema'
+import { metrics } from '../../util/metrics'
+import { Unit } from 'aws-embedded-metrics'
+import { DUTCHV2_ORDER_LATENCY_THRESHOLD_SEC } from '../constants'
 
 export class PostOrderBodyParser {
   private readonly uniswapXParser = new UniswapXOrderParser()
@@ -95,6 +98,18 @@ export class PostOrderBodyParser {
   ): DutchV2Order {
     try {
       const order = CosignedV2DutchOrder.parse(encodedOrder, chainId)
+      // Log the decay start time difference for debugging
+      const decayStartTime = order.info.cosignerData.decayStartTime
+      const currentTime = Math.floor(Date.now() / 1000) // Convert to seconds
+      const timeDifference = currentTime - Number(decayStartTime)
+
+      if (timeDifference > DUTCHV2_ORDER_LATENCY_THRESHOLD_SEC) {
+        const staleOrderMetricName = `StaleOrder-chain-${chainId.toString()}`
+        metrics.putMetric(staleOrderMetricName, 1, Unit.Count)
+      }
+      const staleOrderMetricName = `OrderStaleness-chain-${chainId.toString()}`
+      metrics.putMetric(staleOrderMetricName, timeDifference)
+
       return new DutchV2Order(order as SDKV2DutchOrder, signature, chainId, undefined, undefined, quoteId, requestId)
     } catch (err) {
       this.logger.error('Unable to parse DutchV2 order', {
