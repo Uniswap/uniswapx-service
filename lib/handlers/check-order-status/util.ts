2ad6676a653a482ed6ad8512e435d5b1110e31db
Cody Born <cody.born@uniswap.org>
2024-11-27 18:18:51 +0900

    Resolve Dutchv3 amounts

--- lib/handlers/check-order-status/util.ts
+++ lib/handlers/check-order-status/util.ts
@@ -30,6 +30,8 @@ export function getSettledAmounts(
 ) {
   if (parsedOrder instanceof DutchOrder || parsedOrder instanceof CosignedV2DutchOrder) {
     return getDutchSettledAmounts(fill, metadata.timestamp, parsedOrder)
+  } else if (parsedOrder instanceof CosignedV3DutchOrder) {
+    return getDutchV3SettledAmounts(fill, parsedOrder)
   } else if (parsedOrder instanceof CosignedPriorityOrder) {
     return getPrioritySettledAmounts(fill, metadata, parsedOrder)
   } else {
@@ -80,6 +82,73 @@ export function getPrioritySettledAmounts(
         amountIn,
         tokenOut: nativeOutput.token,
         amountOut: nativeOutput.amount.toString(),
+      })
+    })
+  }
+
+  fill.outputs.forEach((output) => {
+    settledAmounts.push({
+      tokenIn: parsedOrder.info.input.token,
+      amountIn,
+      tokenOut: output.token,
+      amountOut: output.amount.toString(),
+    })
+  })
+
+  return settledAmounts
+}
+
+/**
+ * get the ammounts transfered on chain
+ * used for logging
+ */
+export function getDutchV3SettledAmounts(
+  fill: FillInfo,
+  parsedOrder: CosignedV3DutchOrder
+): SettledAmount[] {
+  parsedOrder.resolve({currentBlock: fill.blockNumber})
+  const nativeOutputs = parsedOrder.info.outputs.filter((output) => output.token.toLowerCase() === NATIVE_ADDRESS)
+  const settledAmounts: SettledAmount[] = []
+  let amountIn: string
+
+  if (!parsedOrder.info.input.curve || 
+    (parsedOrder.info.input.curve.relativeAmounts.length === 1 || 
+    parsedOrder.info.input.curve.relativeAmounts[0] === BigInt(0))) {
+    // If the order is EXACT_INPUT then the input will not decay and resolves to the startAmount/endAmount.
+    amountIn = parsedOrder.info.input.startAmount.toString()
+
+    // Resolve the native outputs using the fill block number and filler address from the fill log.
+    // This will give us a minimum resolved amount for native out swaps.
+    const resolvedOrder = parsedOrder.resolve({ currentBlock: fill.blockNumber, filler: fill.filler })
+    const resolvedNativeOutputs = resolvedOrder.outputs.filter(
+      (output) => output.token.toLowerCase() === NATIVE_ADDRESS
+    )
+
+    // Add all the resolved native outputs to the settledAmounts as they are not included in the fill logs.
+    resolvedNativeOutputs.forEach((resolvedNativeOutput) => {
+      settledAmounts.push({
+        tokenIn: parsedOrder.info.input.token,
+        amountIn,
+        tokenOut: resolvedNativeOutput.token,
+        amountOut: resolvedNativeOutput.amount.toString(),
+      })
+    })
+  } else {
+    // If the order is EXACT_OUTPUT we will have all the ERC20 transfers in the fill logs,
+    // only log the amountIn that matches the order input token.
+
+    // eslint-disable-next-line  @typescript-eslint/no-non-null-assertion
+    const input = fill.inputs.find((input) => input.token.toLowerCase() === parsedOrder.info.input.token.toLowerCase())!
+    amountIn = input.amount.toString()
+
+    // Add all the native outputs to the settledAmounts as they are not included in the fill logs.
+    // The amount is just the startAmount because the order is EXACT_OUTPUT so there is no decay on the outputs.
+    nativeOutputs.forEach((nativeOutput) => {
+      settledAmounts.push({
+        tokenIn: parsedOrder.info.input.token,
+        amountIn,
+        tokenOut: nativeOutput.token,
+        amountOut: nativeOutput.startAmount.toString(),
       })
     })
   }
