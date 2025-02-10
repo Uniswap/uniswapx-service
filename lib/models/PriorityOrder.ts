cff1f8c0d8a19e53f9cab76db6e3b53dcc10d527
Zach Yang <zhiyuan.zach.yang@gmail.com>
2025-02-10 14:10:05 -0500

    Revert "feat, test: store and log classic blockNumber (#520)"
    This reverts commit 9869df4736b04fae91fad7201ddbcb9f20951b9e.

--- lib/models/PriorityOrder.ts
+++ lib/models/PriorityOrder.ts
@@ -63,7 +63,6 @@ export class PriorityOrder extends Order {
       createdAt: this.createdAt,
       referencePrice: quoteMetadata?.referencePrice,
       priceImpact: quoteMetadata?.priceImpact,
-      blockNumber: quoteMetadata?.blockNumber,
       route: quoteMetadata?.route,
       pair: quoteMetadata?.pair,
     }
