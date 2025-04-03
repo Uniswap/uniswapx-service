bb12440543b0c8992cd02d2a8dea8e0513edebe2
Cody Born <cody.born@uniswap.org>
2025-04-02 23:57:21 -0400

    Use generics (#546)

--- lib/util/unimind.ts
+++ lib/util/unimind.ts
@@ -8,14 +8,14 @@ export function unimindAddressFilter(address: string) {
   return address.toLowerCase() === UNIMIND_DEV_SWAPPER_ADDRESS.toLowerCase()
 }
 
-export interface IUnimindAlgorithm {
+export interface IUnimindAlgorithm<T> {
   /**
  * @notice Adjusts Unimind parameters (intrinsic values) based on historical order statistics
  * @param statistics Aggregated order data containing arrays of wait times, fill statuses, and price impacts
  * @param pairData Previous parameters intrinsic values stored for the pair
  * @return Updated intrinsic parameters
  */
-  unimindAlgorithm(statistics: UnimindStatistics, pairData: UnimindParameters, log: Logger): any;
-  computePi(intrinsicValues: any, extrinsicValues: QuoteMetadata): number;
-  computeTau(intrinsicValues: any, extrinsicValues: QuoteMetadata): number;
+  unimindAlgorithm(statistics: UnimindStatistics, pairData: UnimindParameters, log: Logger): T;
+  computePi(intrinsicValues: T, extrinsicValues: QuoteMetadata): number;
+  computeTau(intrinsicValues: T, extrinsicValues: QuoteMetadata): number;
 }
