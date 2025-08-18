0a54f2541981d81c55b127a1cf7a73c65a0115bc
Zach Yang <zhiyuan.zach.yang@gmail.com>
2025-08-18 18:48:41 -0400

    Revert "feat, test: immediately notify exclusive filler" (#599)
    Revert "feat, test: immediately notify exclusive filler (#595)"
    
    This reverts commit 401eb5e63a04166856cf52b5309abb16f54b67bf.

--- test/unit/providers/json-webhook-provider.test.ts
+++ test/unit/providers/json-webhook-provider.test.ts
@@ -20,17 +20,3 @@ describe('JsonWebHookProvider test', () => {
     ).toEqual([{ url: 'webhook.com/1' }, { url: 'webhook.com/2' }, { url: 'webhook.com/4' }])
   })
 })
-
-describe('getExclusiveFillerEndpoints', () => {
-  it('Returns endpoints for a filler', async () => {
-    const webhookProvider = JsonWebhookProvider.create({
-      filter: {
-        filler: {
-          '0x1': [{ url: 'webhook.com/1' }],
-        },
-        orderStatus: { open: [{ url: 'webhook.com/2' }, { url: 'webhook.com/3' }] },
-      },
-    } as any)
-    expect(await webhookProvider.getExclusiveFillerEndpoints('0x1')).toEqual([{ url: 'webhook.com/1' }])
-  })
-})
