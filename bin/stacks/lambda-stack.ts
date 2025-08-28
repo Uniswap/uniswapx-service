bea06f0b24841fc7a9ff039f858d869bf56e7bfb
Alan Wu <alanwu100@gmail.com>
2025-08-28 20:55:07 +0800

    feat: bump getOrders lambda memory

--- bin/stacks/lambda-stack.ts
+++ bin/stacks/lambda-stack.ts
@@ -139,7 +139,7 @@ export class LambdaStack extends cdk.NestedStack {
       entry: path.join(__dirname, '../../lib/handlers/get-orders/index.ts'),
       handler: 'getOrdersHandler',
       timeout: Duration.seconds(29),
-      memorySize: 512,
+      memorySize: 1024,
       bundling: {
         minify: true,
         sourceMap: true,
