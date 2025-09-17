f554347d202138ddd49802f8544d49ac21d64b7d
Alan Wu <alanwu100@gmail.com>
2025-09-17 13:19:03 -0700

    refactor: align event producer and consumer constants

--- bin/constants.ts
+++ bin/constants.ts
@@ -1,3 +1,5 @@
+import { ANALYTICS_EVENTS } from '../lib/util/analytics-events'
+
 // IMPORANT: Once this has been changed once from the original value of 'Template',
 // do not change again. Changing would cause every piece of infrastructure to change
 // name, and thus be redeployed. Should be camel case and contain no non-alphanumeric characters.
@@ -7,9 +9,9 @@ export const UNIMIND_ALGORITHM_CRON_INTERVAL = 15 // minutes
 
 // CloudWatch Logs subscription filter patterns
 export const FILTER_PATTERNS = {
-  ORDER_POSTED: '{ $.eventType = "OrderPosted" }',
-  UNIMIND_RESPONSE: '{ $.eventType = "UnimindResponse" }',
-  UNIMIND_PARAMETER_UPDATE: '{ $.eventType = "UnimindParameterUpdate" }',
+  ORDER_POSTED: `{ $.eventType = "${ANALYTICS_EVENTS.ORDER_POSTED}" }`,
+  UNIMIND_RESPONSE: `{ $.eventType = "${ANALYTICS_EVENTS.UNIMIND_RESPONSE}" }`,
+  UNIMIND_PARAMETER_UPDATE: `{ $.eventType = "${ANALYTICS_EVENTS.UNIMIND_PARAMETER_UPDATE}" }`,
   TERMINAL_ORDER_STATE: '{ $.orderInfo.orderStatus = "filled" || $.orderInfo.orderStatus = "cancelled" }',
   INSUFFICIENT_FUNDS: '{ $.orderInfo.orderStatus = "insufficient-funds" }',
 } as const
